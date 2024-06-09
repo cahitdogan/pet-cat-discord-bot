1b183f85eb58372310ea3d4139668efc32af91a1
cahit.dogan@outlook.com.tr <cahit.dogan@outlook.com.tr>
2024-06-09 19:27:06 +0300

    prefix command handler

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,7 @@
-const { collection, setDoc, doc, getDocFromServer, getDocsFromServer } = require("firebase/firestore");
-const db = require("@root/firebase");
+const { doc, getDocFromServer } = require("firebase/firestore");
+const { db, guildsCollectionRef, usersCollectionRef } = require("@root/firebase.js");
+const { pet } = require("@root/prefix-commands/pet.js");
+
 
 module.exports = {
     name: "messageCreate",
@@ -8,7 +10,7 @@ module.exports = {
         
         let guildPrefix;
         try {
-            const guildDocRef = doc(db, "guilds", message.guildId);
+            const guildDocRef = doc(guildsCollectionRef, message.guildId);
             const guildDocSnap = await getDocFromServer(guildDocRef);
             guildPrefix = guildDocSnap.data().prefix;
         } catch (error) {
@@ -18,6 +20,38 @@ module.exports = {
 
         if (!message.content.startsWith(prefix + " ")) return;
 
-        console.log("Success!");
+
+        const endCharacter = Number(message.content.slice(-1));
+        
+        console.log(endCharacter);
+
+        let command;
+        let index = 0;
+        if (isNaN(endCharacter)) {
+            command = message.content + " ";
+        } 
+        else if (endCharacter >= 2) { 
+            command = message.content.slice(0, -1);
+            index = endCharacter - 1;
+        }
+        else {
+            await message.reply("Number must be 2 or greater than 2 (max 9)");
+            return;
+        }
+
+        const authorDocRef = doc(usersCollectionRef, message.author.id);
+        const authorDocSnap = await getDocFromServer(authorDocRef);
+        const authorData = authorDocSnap.data();
+        const pet = authorData.pets[index];
+
+        switch (command) {
+            case `${prefix} pet `:
+                await message.reply("Başarılı");
+                break;
+        
+            default:
+                await message.reply("There is no such command. (You can have a maximum of 9 pets)");
+                break;
+        }
     }
 }
