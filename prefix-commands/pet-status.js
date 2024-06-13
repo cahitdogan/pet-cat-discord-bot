53ee1f79a404570fbddc1420303bb4a3755dcb92
cahit.dogan@outlook.com.tr <cahit.dogan@outlook.com.tr>
2024-06-13 10:59:57 +0300

    completed

---
+++ prefix-commands/pet-status.js
@@ -0,0 +1,40 @@
+const {  EmbedBuilder } = require("discord.js");
+const { getDoc, doc } = require("firebase/firestore/lite");
+const { db } = require("@root/firebase.js");
+
+module.exports = {
+    async petStatus(message, petDocRef, userName) {
+        try {
+            const petDocSnap = await getDoc(petDocRef);
+            const petData = petDocSnap.data();
+            const { level, health, petName } = petData;
+            const { water, food, sleep, play, shower, toilet} = petData.stats;
+
+            const petEmbed = new EmbedBuilder()
+                .setColor("#FECEDE")
+                .setTitle(`${petName}`)
+                .setImage('https://cdn.discordapp.com/attachments/1247190694481756160/1248683005447508118/Group_13.png?ex=66648e2b&is=66633cab&hm=2ee06ebf27971955f989cfa358fcb6bcb0489b16b56bcbbe1d8f6b8b3853fed2&')
+                .addFields(
+                    { name: 'Water 💧', value: `${water.value}`, inline: true },
+                    { name: 'Food 🥫', value: `${food.value}`, inline: true },
+                    { name: 'Sleep 💤', value: `${sleep.value}`, inline: true },
+                )
+                .addFields(
+                    { name: 'Play 🧶', value: `${play.value}`, inline: true },
+                    { name: 'Shower 🧼', value: `${shower.value}`, inline: true },
+                    { name: 'Toilet 💩', value: `${toilet.value}`, inline: true },
+                )
+                .addFields(
+                    { name: '\u200B', value: `🏅**Lvl:** ${level}`, inline: true },
+                    { name: '\u200B', value: `❤️ **Health:** ${health}`, inline: true }
+                )
+                .setFooter({text: `${userName}`})
+                .setTimestamp();
+
+            await message.channel.send({ embeds: [petEmbed] });
+        } catch (error) {
+            console.error(error);
+            await message.channel.send("An error occurred while viewing your pet.");
+        }
+    }
+}
