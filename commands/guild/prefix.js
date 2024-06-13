c8244b8839123c03f66742ebfd2015fde61c76cd
cahit.dogan@outlook.com.tr <cahit.dogan@outlook.com.tr>
2024-06-13 15:44:48 +0300

    to set custom server prefix

---
+++ commands/guild/prefix.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { db } = require("@root/firebase.js");
+const { updateDoc, doc } = require("firebase/firestore/lite");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("prefix")
+        .setDescription("Set a custom prefix for this server.")
+        .addStringOption(option => 
+            option.setName("prefix")
+                .setMaxLength(7)
+                .setDescription("WARNING: The prefix must start with a dot.")
+                .setRequired(true))
+        
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+    async execute(interaction) {
+        const prefix = interaction.options.getString("prefix");
+
+        if (!prefix.startsWith(".")) {
+            await interaction.reply("Error! The prefix must start with a dot.");
+            return;
+        }
+
+        const space = prefix.search(" ");
+
+        if (space !== -1) {
+            await interaction.reply("Error! The prefix cannot contain spaces.");
+            return;
+        }
+        
+        const guildID = interaction.guildId;
+        const guildDocRef = doc(db, "guilds", guildID);
+        await updateDoc(guildDocRef, { prefix: prefix });
+
+        await interaction.reply("Prefix set successfully.");
+    }
+}
