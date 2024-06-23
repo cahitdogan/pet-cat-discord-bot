92317bf327631a316bf232b153c83bd1a9081e8a
cahit.dogan@outlook.com.tr <cahit.dogan@outlook.com.tr>
2024-06-23 09:11:53 +0300

    feat: Add my-pets command functionality

---
+++ commands/user/my-pets.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { doc, getDoc } = require("firebase/firestore/lite");
+const { db } = require("../../firebase.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("my-pets")
+        .setDescription("Shows your pet list."),
+
+    async execute(interaction) {
+        const userID = interaction.user.id;
+        const userName = interaction.user.username
+        const userDocRef = doc(db, "users", userID);
+        const userDocSnap = await getDoc(userDocRef);
+
+        if (!userDocSnap.exists()) {
+            await interaction.reply(`<@${userID} | You have not a pet.`);
+            return;
+        }
+
+        const pets = userDocSnap.get("pets");
+
+        if (pets.length === 0) {
+            await interaction.reply(`<@${userID} | You have not a pet.`);
+            return;
+        }
+
+        let petListString = "";
+
+        for (let i = 0; i < pets.length; i++) {
+            petListString += `${i + 1}. ` + pets[i].petName + "\n";
+        }
+        
+        const petsEmbed = new EmbedBuilder()
+            .setColor("#FECEDE")
+            .addFields({name: `${userName}'s Pets`, value: petListString})
+            .setFooter({text: `${userName}`})
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [petsEmbed] });
+    }
+}
