3f79ad280d01d9d3d2ea60a3dc9ea5fe9b5d74b5
Cahit <103262027+cahitdogan@users.noreply.github.com>
2024-08-22 19:31:55 +0300

    add owner state info

--- commands/user/my-pets.js
+++ commands/user/my-pets.js
@@ -3,40 +3,42 @@ const { doc, getDoc } = require("firebase/firestore/lite");
 const { db } = require("../../firebase.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("my-pets")
-        .setDescription("Shows your pet list."),
+  data: new SlashCommandBuilder()
+    .setName("my-pets")
+    .setDescription("Shows your pet list."),
 
-    async execute(interaction) {
-        const userID = interaction.user.id;
-        const userName = interaction.user.username
-        const userDocRef = doc(db, "users", userID);
-        const userDocSnap = await getDoc(userDocRef);
+  async execute(interaction) {
+    const userID = interaction.user.id;
+    const userName = interaction.user.username
+    const userDocRef = doc(db, "users", userID);
+    const userDocSnap = await getDoc(userDocRef);
 
-        if (!userDocSnap.exists()) {
-            await interaction.reply(`<@${userID}> | You have not a pet.`);
-            return;
-        }
-
-        const pets = userDocSnap.get("pets");
-
-        if (pets.length === 0) {
-            await interaction.reply(`<@${userID}> | You have not a pet.`);
-            return;
-        }
-
-        let petListString = "";
-
-        for (let i = 0; i < pets.length; i++) {
-            petListString += `${i + 1}. ` + pets[i].petName + "\n";
-        }
-        
-        const petsEmbed = new EmbedBuilder()
-            .setColor("#FECEDE")
-            .addFields({name: `${userName}'s Pets`, value: petListString})
-            .setFooter({text: `${userName}`})
-            .setTimestamp();
-
-        await interaction.reply({ embeds: [petsEmbed] });
+    if (!userDocSnap.exists()) {
+      await interaction.reply(`<@${userID}> | You have not a pet.`);
+      return;
     }
+
+    const pets = userDocSnap.get("pets");
+
+    if (pets.length === 0) {
+      await interaction.reply(`<@${userID}> | You have not a pet.`);
+      return;
+    }
+
+    let petListString = "";
+
+    for (let i = 0; i < pets.length; i++) {
+      const ownerState = pets[i].ownerState;
+      const ownerStateString = ownerState ? " (Owner)" : ' (Partner)';
+      petListString += `${i + 1}. ` + pets[i].petName + ownerStateString + "\n";
+    }
+
+    const petsEmbed = new EmbedBuilder()
+      .setColor("#FECEDE")
+      .addFields({ name: `${userName}'s Pets`, value: petListString })
+      .setFooter({ text: `${userName}` })
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [petsEmbed] });
+  }
 }
